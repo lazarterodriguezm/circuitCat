94a937571f51608a36612df963f3596869a8676d
Marc <lazarterodriguezm@gmail.com>
2018-11-05 15:30:39 +0100

    Implemented the operation of the menu slides in the database.

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { StorageProvider } from './../../providers/storage/storage';
+
+import { Observable } from 'rxjs/Observable';
+
 @IonicPage()
 @Component({
   selector: 'page-menu',
@@ -8,7 +12,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  entrees: Observable<any[]>;
+  mainCourses: Observable<any[]>;
+  desserts: Observable<any[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storageProvider: StorageProvider) {
+    this.entrees = this.storageProvider.getListFromDatabase('menus/entrantes');
+    this.mainCourses = this.storageProvider.getListFromDatabase('menus/platos principales');
+    this.desserts = this.storageProvider.getListFromDatabase('menus/postres');
   }
 
   ionViewDidLoad() {
