93f045d21902a0770ab0ad10ea11c172377e8c90
Marc <lazarterodriguezm@gmail.com>
2018-11-05 14:54:01 +0100

    Implemented the operation of the home slides in the database.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { StorageProvider } from './../../providers/storage/storage';
+
+import { Observable } from 'rxjs/Observable';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -8,7 +12,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  covers: Observable<any[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storageProvider: StorageProvider) {
+    this.covers = this.storageProvider.getListFromDatabase('portadas');
   }
 
   ionViewDidLoad() {
