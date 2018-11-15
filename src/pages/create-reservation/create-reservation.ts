bb2701660293e644cf90f9ed827303286b95b7be
Marc <lazarterodriguezm@gmail.com>
2018-11-15 15:48:52 +0100

    Replacing DateTime by DatePicker on CreateReservationPage

--- src/pages/create-reservation/create-reservation.ts
+++ src/pages/create-reservation/create-reservation.ts
@@ -17,8 +17,9 @@ import { DatePicker } from '@ionic-native/date-picker';
 })
 export class CreateReservationPage {
 
-  reservation = {hora: "", fecha: "", uuid: ""}
-  options = { year: '2-digit', month: '2-digit', day: '2-digit' };
+  options = { hour: '2-digit', minute: '2-digit', day: '2-digit', month: '2-digit', year: '2-digit' };
+
+  reservation = {fecha: "", uuid: ""}
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private storageProvider: StorageProvider, private toastCtrl: ToastController, private device: Device, private datePicker: DatePicker) {
     this.reservation.fecha = (new Date).toLocaleDateString('es-ES', this.options);
@@ -30,9 +31,28 @@ export class CreateReservationPage {
 
   showDatePicker() {
     this.datePicker.show({
-      date: new Date(),
-      mode: 'date',
-      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+      mode: 'datetime',
+      date: new Date,
+      minDate: (new Date).setDate((new Date).getDate() - 0),
+      maxDate: (new Date).setDate((new Date).getDate() + 90),
+      titleText: "Fecha",
+      okText: "Aceptar",
+      cancelText: "Cancelar",
+      todayText: "",
+      nowText: "",
+      is24Hour: true,
+      androidTheme: 5,
+      allowOldDates: true,
+      allowFutureDates: true,
+      doneButtonLabel: "Aceptar",
+      doneButtonColor: "#e61414",
+      cancelButtonLabel: "Cancelar",
+      cancelButtonColor: "#bf1111",
+      x: 0,
+      y: 0,
+      minuteInterval: 30,
+      popoverArrowDirection: "any",
+      locale: "es_ES"
     }).then(
       date => this.reservation.fecha = date.toLocaleDateString('es-ES', this.options),
       err => console.log('Error occurred while getting date: ', err)
@@ -46,7 +66,7 @@ export class CreateReservationPage {
   createReservation() {
     this.reservation.uuid = this.device.uuid;
 
-    this.storageProvider.storeInfoToDatabase('reservasUsuarios/' + this.device.uuid, this.device.uuid + this.reservation.hora + this.reservation.fecha, this.reservation).then(() => {
+    this.storageProvider.storeInfoToDatabase('reservasUsuarios/' + this.device.uuid, this.device.uuid + this.reservation.fecha, this.reservation).then(() => {
       let toast = this.toastCtrl.create({
         message: 'New File added!',
         duration: 3000
